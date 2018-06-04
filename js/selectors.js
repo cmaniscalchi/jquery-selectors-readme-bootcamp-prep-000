65b8b2572fbd0fc9f4261e81df9e264fc2a9f565
Claudia Maniscalchi <claudia.maniscalchi@gmail.com>
2018-06-04 03:31:35 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
-// declare your functions here...
+$(document).ready(function() {
+  function paragraphSelector() {
+    return $('p');
+  }
+  
+  function lastImageSelector() {
+    $('img:last');
+  }
+  
+  function ninjaBabySelector() {
+    return $('#baby-ninja');
+  }
+  
+  function divSelector() {
+    return $('.pics');
+  }
+  
+  function firstListItem() {
+    return $('#pic-list ul li');
+  }
+});
